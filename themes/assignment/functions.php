49945ebcab9d66f42de066494c6c91266f9f06da
superbwebdeveloper <superbwebdeveloper@users.noreply.github.com>
2020-01-17 11:58:01 +0530

    Update functions.php

--- themes/assignment/functions.php
+++ themes/assignment/functions.php
@@ -1,1 +1,9 @@
-if (!file_exists(get_template_directory() . '/class-wp-bootstrap-navwalker.php')) {     // File does not exist... return an error.     return new WP_Error('class-wp-bootstrap-navwalker-missing', __('It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker')); } else {     // File exists... require it.     require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php'; } 
+<?php
+if (!file_exists(get_template_directory() . '/class-wp-bootstrap-navwalker.php')) {
+	// File does not exist... return an error.
+	return new WP_Error('class-wp-bootstrap-navwalker-missing', __('It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker'));
+} else {
+	// File exists... require it.
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+?>
